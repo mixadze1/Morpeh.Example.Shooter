dec44d88a9d2f85e28627cbda6fd2d9288770058
Mikhail A <mixadze1@bk.ru>
2025-07-29 15:25:49 +0300

    [add] change weapon config so on more beaty view

---
+++ Assets/_Scripts/Core/Configs/WeaponConfigs/WeaponConfig.cs
@@ -0,0 +1,40 @@
+﻿using _Scripts.Core.Providers.WeaponProviders;
+using AYellowpaper.SerializedCollections;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _Scripts.Core.Configs.WeaponConfigs
+{
+    [CreateAssetMenu(menuName = "Configs/Weapon", fileName = nameof(WeaponConfig))]
+    public class WeaponConfig : ScriptableObject
+    {
+        [BoxGroup("General")]
+        [SerializeField, Range(5, 1500), LabelText("Bullet speed m/s")] private float _speedBullet;
+
+        [BoxGroup("General")]
+        [SerializeField, Range(0.005f, 5f)] private float _shootPerSecond;
+
+        [BoxGroup("General"), SerializeField, LabelText("Magazine Settings")] 
+        private WeaponMagazineConfig _magazineConfig;
+
+        [Title("References")]
+        [LabelText("Bullet Prefab"), Required]
+        [SerializeField] private BulletProvider _bulletPrefab;
+
+        [LabelText("Player Weapon Preset"), Required]
+        [SerializeField] private PlayerWeaponProvider _playerWeaponPreset;
+
+        [LabelText("Decal Provider (Wall)"), Required]
+        [SerializeField] private DecalProvider _decalWallProvider;
+
+        [SerializedDictionary("Trigger", "Animation")]
+        public SerializedDictionary<Trigger, AnimationData> AnimationConfig;
+
+        public float SpeedBullet => _speedBullet;
+        public float ShootPerSecond => _shootPerSecond;
+        public BulletProvider BulletPrefab => _bulletPrefab;
+        public PlayerWeaponProvider WeaponPreset => _playerWeaponPreset;
+        public DecalProvider DecalWallProvider => _decalWallProvider;
+        public WeaponMagazineConfig MagazineConfig => _magazineConfig;
+    }
+}
--- Assets/_Scripts/Core/Configs/WeaponConfigs/WeaponsConfig.cs
+++ Assets/_Scripts/Core/Configs/WeaponConfigs/WeaponsConfig.cs
@@ -18,24 +18,6 @@ namespace _Scripts.Core.Configs.WeaponConfigs
         public float LifetimeDecal => _lifetimeDecal;
     }
 
-    [Serializable]
-    public class WeaponConfig
-    {
-        [SerializeField] private float _speedBullet;
-        [SerializeField] private float _shootPerSecond;
-        [SerializeField] private BulletProvider _bulletPrefab; 
-        [SerializeField] private PlayerWeaponProvider _playerWeaponPreset;
-        [SerializeField] private WeaponMagazineConfig _magazineConfig;
-        [SerializeField] private DecalProvider _decalWallProvider;
-        
-        public DecalProvider DecalWallProvider => _decalWallProvider;
-        public float SpeedBullet => _speedBullet;   
-        public BulletProvider BulletPrefab => _bulletPrefab;
-        public PlayerWeaponProvider WeaponPreset => _playerWeaponPreset;
-        public WeaponMagazineConfig MagazineConfig => _magazineConfig;
-        public SerializedDictionary<Trigger, AnimationData> AnimationConfig;
-        public float ShootPerSecond => _shootPerSecond;
-    }
 
     public enum TypeWeapon
     {
